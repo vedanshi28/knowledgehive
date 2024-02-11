1a650358f6d496605d51bcdcbdfa8418c41d81c6
vedanshi_28 <vedanshimishra844@gmail.com>
2024-02-11 19:08:06 +0530

    commit changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar=()=>{
              <div className="col-10 mx-auto">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
-           <a className="navbar-brand" href="#">Knowledge Hive</a>
+           <a className="navbar-brand" href="#"><h5>Knowledge Hive</h5></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
            </button>
